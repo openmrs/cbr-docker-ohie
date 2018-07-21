21f9b6c895b9bc818fd4d0b2887bbb75f1cd45f2
wyclif <wyclif@openmrs.org>
2018-07-20 20:27:52 -0400

    Added simulator

---
+++ simulator/config.js
@@ -0,0 +1,19 @@
+
+var config = {
+    openmrsInstances : [
+        {
+            id: 1,
+            name: "OpenMRS Instance 1",
+            baseUrl: "http://localhost:8080/openmrs",
+            username: 'admin',
+            patientIdentifierTypeUuid: '8d79403a-c2cc-11de-8d13-0010c6dffd0f'
+        },
+        {
+            id: 2,
+            name: "OpenMRS Instance 2",
+            baseUrl: "http://localhost:8081/openmrs",
+            username: 'admin',
+            patientIdentifierTypeUuid: '8d79403a-c2cc-11de-8d13-0010c6dffd0f'
+        }
+    ]
+}
---
+++ simulator/simulator-dataset-short.js
@@ -0,0 +1,119 @@
+var dataset = {
+    "patients":[
+        {
+            "identifier": "926RT",
+            "givenName": "Eve",
+            "middleName": "Samual",
+            "familyName": "Dion",
+            "gender": "M",
+            "birthdate": -11511
+        },
+        {
+            "identifier": "7LGKP",
+            "givenName": "Caden",
+            "middleName": "Britni",
+            "familyName": "Ruthann",
+            "gender": "M",
+            "birthdate": -13680
+        },
+        {
+            "identifier": "8AEUX",
+            "givenName": "Eliza",
+            "middleName": "Atlas",
+            "familyName": "Sabrina",
+            "gender": "M",
+            "birthdate": -12573
+        },
+        {
+            "identifier": "7WU9G",
+            "givenName": "Leland",
+            "middleName": "Craig",
+            "familyName": "Marna",
+            "gender": "M",
+            "birthdate": -9007
+        },
+        {
+            "identifier": "FLPCR",
+            "givenName": "Katelynn",
+            "middleName": "Edmundo",
+            "familyName": "Monte",
+            "gender": "M",
+            "birthdate": -20830
+        }
+    ],
+    "timeline":[
+        {
+            "date": -1844,
+            "event": "artStartDate",
+            "identifier": "926RT"
+        },
+        {
+            "date": -1844,
+            "event": "cd4Count",
+            "identifier": "926RT",
+            "value": "109"
+        },
+        {
+            "date": -1844,
+            "event": "viralLoad",
+            "identifier": "926RT",
+            "value": "812"
+        },
+        {
+            "date": -1832,
+            "event": "cd4Count",
+            "identifier": "7LGKP",
+            "value": "543"
+        },
+        {
+            "date": -1832,
+            "event": "viralLoad",
+            "identifier": "7LGKP",
+            "value": "734"
+        },
+        {
+            "date": -1823,
+            "event": "artStartDate",
+            "identifier": "8AEUX"
+        },
+        {
+            "date": -1823,
+            "event": "cd4Count",
+            "identifier": "8AEUX",
+            "value": "602"
+        },
+        {
+            "date": -1733,
+            "event": "viralLoad",
+            "identifier": "8AEUX",
+            "value": "4999"
+        },
+        {
+            "date": -1763,
+            "event": "reasonArtStopped",
+            "identifier": "8AEUX"
+        },
+        {
+            "date": -1793,
+            "event": "viralLoad",
+            "identifier": "8AEUX",
+            "value": "5001"
+        },
+        {
+            "date": -1823,
+            "event": "deathdate",
+            "identifier": "8AEUX"
+        },
+        {
+            "date": -1818,
+            "event": "artStartDate",
+            "identifier": "7WU9G"
+        },
+        {
+            "date": -1818,
+            "event": "cd4Count",
+            "identifier": "7WU9G",
+            "value": "1546"
+        }
+    ]
+}

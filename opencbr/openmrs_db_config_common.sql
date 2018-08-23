697d2cfc4989fcfb7be4ac87b39d5e2ddd509c46
wluyima <wyclif@openmrs.org>
2018-08-23 15:53:05 -0400

    Set a uniform repeat interval of 15sec for the CBR tasks

--- opencbr/openmrs_db_config_common.sql
+++ opencbr/openmrs_db_config_common.sql
@@ -3,12 +3,14 @@ SET start_on_startup = true
 WHERE
   name IN ('New HIV Case', 'New HIV Treatment', 'HIV Treatment Failure', 'HIV Treatment Stopped', 'HIV Patient Died');
 
-UPDATE scheduler_task_config SET repeat_interval = 8 WHERE name = 'New HIV Case';
+SET @INTERVAL = 15;
 
-UPDATE scheduler_task_config SET repeat_interval = 11 WHERE name = 'New HIV Treatment';
+UPDATE scheduler_task_config SET repeat_interval = @INTERVAL WHERE name = 'New HIV Case';
 
-UPDATE scheduler_task_config SET repeat_interval = 14 WHERE name = 'HIV Treatment Failure';
+UPDATE scheduler_task_config SET repeat_interval = @INTERVAL WHERE name = 'New HIV Treatment';
 
-UPDATE scheduler_task_config SET repeat_interval = 17 WHERE name = 'HIV Treatment Stopped';
+UPDATE scheduler_task_config SET repeat_interval = @INTERVAL WHERE name = 'HIV Treatment Failure';
 
-UPDATE scheduler_task_config SET repeat_interval = 20 WHERE name = 'HIV Patient Died';
+UPDATE scheduler_task_config SET repeat_interval = @INTERVAL WHERE name = 'HIV Treatment Stopped';
+
+UPDATE scheduler_task_config SET repeat_interval = @INTERVAL WHERE name = 'HIV Patient Died';
